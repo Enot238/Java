e9c0f4edb2dd4fe7eacba07983a15fc466341d85
Phill <57107370+Enot238@users.noreply.github.com>
2022-05-13 16:07:09 +0300

    Update Laba7.java

--- Laba7.java
+++ Laba7.java
@@ -1,164 +1,186 @@
 package com.company;
 import java.io.*;
-import java.time.temporal.Temporal;
 import java.util.*;
 
+import static com.company.Station.stations;
 
+abstract class Tramvai_way implements Serializable
+        {
+            protected int nomer;
+            protected float argintervalmove;
+            public Tramvai_way(){}
+        }
+class Station extends Tramvai_way implements Serializable {
+    private String name;
+    private int countpas;
+    public static List<Station> stations = new ArrayList<>();
+    public static List<Station> stations1 = new ArrayList<>();
 
-abstract class Mus_band implements Serializable{
-    protected String Name_band;
-    protected String surname;
-    public Mus_band(){};
-}
-
-class Trip extends Mus_band implements Serializable{
-    private String city;
-    private int year;
-    private int consert_count;
-
-    public static List<Trip> trips = new ArrayList<>();
-    public Trip(){};
-
-    public Trip(Trip t)throws IOException{
-        this.Name_band = t.Name_band;
-        this.surname=t.surname;
-        this.city=t.city;
-        this.year=t.year;
-        this.consert_count=t.consert_count;
-        trips.add(this);
-        FileOutputStream writeData = new FileOutputStream("data.txt" );
+    public Station() {
+    }
+    public Station(Station s) throws IOException {
+        this.nomer = s.nomer;
+        this.argintervalmove = s.argintervalmove;
+        this.name = s.name;
+        this.countpas = s.countpas;
+        stations.add(this);
+        FileOutputStream writeData = new FileOutputStream("peopledata.dat");
         ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
-        writeStream.writeObject(trips);
+        writeStream.writeObject(stations);
         writeStream.flush();
         writeStream.close();
     }
 
-    public Trip(String _Name_band, String _surname, String _city, int _year,int _consert_count){
-        Name_band=_Name_band;
-        surname=_surname;
-        city=_city;
-        year=_year;
-        consert_count=_consert_count;
+    public Station(int _nomer, int _argintervalmove, String _name, int _countpas) {
+        nomer=_nomer;
+        argintervalmove=_argintervalmove;
+        name=_name;
+        countpas=_countpas;
     }
 
-    public static com.company.Trip AddTrip(){
+    public static Station AddS() {
         Scanner a = new Scanner(System.in);
-        Scanner c = new Scanner(System.in);
-        com.company.Trip b = new com.company.Trip();
-        try{
-            System.out.print("Введіть назву музичного гурту: ");
-            b.Name_band=a.nextLine();
-            System.out.print("Введіть прізвище керівника: ");
-            b.surname=a.nextLine();
-            System.out.print("Введіть місто: ");
-            b.city=a.nextLine();
-            System.out.print("Введіть рік: ");
-            b.year=c.nextInt();
-            System.out.print("Введіть кількість концертів: ");
-            b.consert_count=c.nextInt();
-        }
-        catch (Exception ex){
+        Scanner с = new Scanner(System.in);
+        Station b = new Station();
+        try {
+            System.out.println("\tВведіть номер маршруту: ");
+            b.nomer = a.nextInt();
+            System.out.println("\tВведіть середній інтервал руху: ");
+            b.argintervalmove = a.nextFloat();
+            System.out.print("\tВведіть назву зупинки: ");
+            b.name = с.nextLine();
+            System.out.print("\tВведіть кількість пасажирів: ");
+            b.countpas = a.nextInt();
+        } catch (Exception ex) {
             System.out.println("Введене некоректні значення!!!");
             System.out.println("Повторіть спробу!!!");
-            return AddTrip();
+            return AddS();
         }
         return b;
     }
-
-    public void OutputTrip(){
-        System.out.println("\tНазву музичного гурту:  " + Name_band);
-        for (String s : Arrays.asList("\tПрізвище керівника: " + surname, "\tМісто: " + city,
-                "\tРік: " + year, "\tКількість концертів: " + consert_count)) {
+    public void OutputTramvai_way(){
+        System.out.println("\tНомер маршруту: " + nomer);
+        for (String s : Arrays.asList("\tСредній інтервал руху'я: " + argintervalmove, "\tНазва зупинки : " + name, "\tКількість пасажирів: " + countpas)) {
             System.out.println(s);
         }
         System.out.println();
     }
 
-    public  static void OutputTrip(List<Trip> array){
+    public static void OutputTramvai_way(List<Station> array) {
         System.out.println("Кількість записів: " + array.size());
         for (int i = 0; i < array.size(); i++) {
-            array.get(i).OutputTrip();
+            array.get(i).OutputTramvai_way();
         }
     }
-
-    public static void FileRead() throws IOException, ClassNotFoundException {
-        try {
-            FileInputStream fis = new FileInputStream("data.txt");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            trips = (List) ois.readObject();
-            ois.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+    public static int Sum(List<Station> stations4)
+    {
+        int sum=0;
+        for(int i=0;i<stations4.size();i++)
+        {
+            sum+=stations4.get(i).countpas;
         }
+        System.out.println("Загальна кількість пасажирів: " + sum);
+        return sum;
     }
-
+    public static int Min(List<Station> stations3)
+    {
+        int min = stations3.get(0).countpas;
+        for(int i = 0; i< stations3.size(); i++)
+        {
+            if(stations3.get(i).countpas<min)
+            {
+                min = stations3.get(i).countpas;
+            }
+        }
+        for(int k = 0; k< stations3.size(); k++)
+        {
+            if(stations3.get(k).countpas==min)
+            {
+               stations3.get(k).OutputTramvai_way();
+            }
+        }
+        System.out.println("Зупинки з найменшою кількістю пасажирів: " + min);
+        return min;
+    }
+    public static int Heig()
+    {
+        int max=stations.get(0).name.toCharArray().length;
+        for(int i=0;i<stations.size();i++)
+        {
+            if(stations.get(i).name.toCharArray().length>max)
+            {
+                max = stations.get(i).name.toCharArray().length;
+            }
+        }
+        for(int k=0;k<stations.size();k++)
+        {
+            if(stations.get(k).name.toCharArray().length==max)
+            {
+                stations.get(k).OutputTramvai_way();
+            }
+        }
+        System.out.println("Запинка з найдовшою назвою: " + max);
+        return max;
+    }
+    public static void DeleteTramvai_way() throws IOException {
+        System.out.println("Введіть номер запису: ");
+        int n = new Scanner(System.in).nextInt();
+        if(n > stations.size()){
+            System.out.println("Номер за межами массиву!");
+            DeleteTramvai_way();
+            return;
+        }
+        stations.remove(n - 1);
+        FileOutputStream writeData = new FileOutputStream("peopledata.dat");
+        ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+        writeStream.writeObject(stations);
+        writeStream.flush();
+        writeStream.close();
+    }
 }
-
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         String n;
         Scanner in = new Scanner(System.in);
         do {
-            System.out.println("============================================================");
-            System.out.println("\t\t---Лабораторна робота №7---");
-            System.out.println("\t\t\t---Список завдань---");
-            System.out.println("---1 - Ввести інформацію про товар ");
-            System.out.println("---2 - Вивести інформацію про товар на складі");
-            System.out.println("---3 - Вивести інформацію складу відсортовану по вазі");
-            System.out.println("---4 - Пошук по назві товару-");
-            System.out.println("---5 - Видалення запису ");
-            System.out.println("---6 - Завершити роботу ");
-            System.out.println("============================================================");
-            System.out.print("Введіть номер завдання: ");
+            System.out.println("1 - Ввести інформацію про трамвайний маршрут.");
+            System.out.println("2 - Вивести інформацію про трамвайний маршрут.");
+            System.out.println("3 - Вивести загальну кількість пасажирів.");
+            System.out.println("4 - Вивести зупунки з найменшою кількістю пасажирів.");
+            System.out.println("5 - Вивести зупинку з найдовшою назвою.");
+            System.out.println("6 - Видалити запис.");
+            System.out.println("7 - Завершити роботу.");
             n = in.next();
             switch (n) {
                 case "1":
-                    Trip trip = new Trip(Trip.AddTrip());
-
+                    Station _Station = new Station(Station.AddS());
                     break;
                 case "2":
-                    Trip.FileRead();
-                    //Trip.OutputTrip(trips);
+                    Station.OutputTramvai_way(stations);
                     break;
                 case "3":
-                    
+
+                    Station.Sum(stations);
                     break;
                 case "4":
-                    
+
+                    Station.Min(stations);
                     break;
                 case "5":
-                    
+
+                    Station.Heig();
                     break;
                 case "6":
+                    Station.DeleteTramvai_way();
+                    break;
+                case "7":
+                    File file = new File("C:\\Users\\38066\\IdeaProjects\\untitled21\\peopledata.dat");
+                    file.deleteOnExit();
                     return;
             }
-        } while (n != "6");
-        return;
+        } while (n != "7");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
