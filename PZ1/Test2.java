86a392f309f7385859a0751aac43106b030caae3
Phill <57107370+Enot238@users.noreply.github.com>
2022-05-18 17:25:08 +0300

    Create Test2.java

---
+++ PZ1/Test2.java
@@ -0,0 +1,10 @@
+package com.company;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class zaav2 {
+    @Test
+    void zav2() {
+        int mas [] =new int[]{1,-1,3,4,-2};
+        assertEquals(7,Main.zav2(mas));
+    }
+}
