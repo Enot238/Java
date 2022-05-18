4ef5e5b384b05d31a74044e66da23355be216a9f
Phill <57107370+Enot238@users.noreply.github.com>
2022-05-18 17:25:31 +0300

    Create Test3.java

---
+++ PZ1/Test3.java
@@ -0,0 +1,9 @@
+package com.company;
+import static org.junit.jupiter.api.Assertions.*;
+class zav1 {
+    @org.junit.jupiter.api.Test
+    void searchMinInArr() {
+        int mas [] =new int[]{1,-1,3,4,-2};
+        assertEquals(-1,Main.SearchMinInArr(mas));
+    }
+}
